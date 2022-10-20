17a13b0495163db9f62b94e31bfaeb0a5370a288
Richard <richard.n.l.green@gmail.com>
2022-10-20 16:24:23 +0100

    Moved latest pico.py to main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
-#Main routine for pico1
-import time
+#Main routine for all picos
+from time import sleep
 import _thread
-import machine # type: ignore
+from machine import reset # type: ignore
 
 #Import my supporting code
 import utils.myid as myid, utils.wifi as wifi, utils.mqtt as mqtt
@@ -14,10 +14,32 @@ print("I am {}".format(pico))
 #Call wifi_connect with our hostname
 wifi_connected = wifi.wlan_connect(pico)
 
+#Send alert 
+def send_mqtt(topic,message):
+    print("{}: {}".format(topic,message))
+    if client != False:
+        mqtt.send_mqtt(client,topic,message)
+
 def restart():
     print('Restarting {} ...'.format(pico))
-    time.sleep(5)
-    machine.reset()
+    sleep(5)
+    reset()
+
+def reload():
+    print("Fetching latest code...")
+    import utils.ftp as ftp
+    topic = 'pico/'+pico+'/status'
+    #Move to the root FTP folder
+    ftp.cwd('pico/scripts')
+    #Get all files for the root
+    numfiles = ftp.get_allfiles(".")
+    message = pico + ' copied ' + str(numfiles) + " files to root"
+    send_mqtt(topic,message)
+    #Get all files for utils (get_allfiles will deal with changing directory)
+    numfiles = ftp.get_allfiles("utils")
+    message = pico + ' copied ' + str(numfiles) + " files to utils"
+    send_mqtt(topic,message)
+    ftp.quit()
 
 #define callback
 def on_message(topic, payload):
@@ -25,36 +47,34 @@ def on_message(topic, payload):
     if str(topic.decode()) == "pico/"+pico+"/control":
         if str(payload.decode()) == "blink":
             blink(0.25,0.25)
+        elif str(payload.decode()) == "reload":
+            reload()
         elif str(payload.decode()) == "restart":
             restart()
         else:
             print("Unknown command: {}".format(str(payload.decode())))
     elif str(topic.decode()) == "pico/"+pico+"/poll":
         heartbeat_topic = "pico/"+pico+"/heartbeat"
-        mqtt.send_mqtt(client,heartbeat_topic,"Yes, I'm here")
+        send_mqtt(heartbeat_topic,"Yes, I'm here")
 
+#Try and connect to MQTT
 client = mqtt.mqtt_connect(client_id=pico)
-if not client:
+
+if client == False:
     print("We should probably reboot now...")
+else:
+    #Say Hello
+    topic = 'pico/'+pico+'/status'
+    message = pico + ' is Alive!'
 
-#Say Hello
-topic = 'pico/'+pico+'/status'
-message = pico + ' is Alive!'
-mqtt.send_mqtt(client,topic,message)
+    send_mqtt(topic,message)
 
-#Initialise the traps
-import trap
+    #Subscribe to control and heartbeat channels
+    client.set_callback(on_message) # type: ignore
+    print("Subscribing to channels...")
+    client.subscribe("pico/"+pico+"/control") # type: ignore
+    client.subscribe("pico/"+pico+"/poll") # type: ignore
 
-#Subscribe to control and heartbeat channels
-client.set_callback(on_message) # type: ignore
-print("Subscribing to channels...")
-client.subscribe("pico/"+pico+"/control") # type: ignore
-client.subscribe("pico/"+pico+"/poll") # type: ignore
-
-while True:
-    #Check the traps
-    trap.trap()
-    #Check for messages
-    client.check_msg() # type: ignore
-    #Wait a bit
-    time.sleep(0.2)
+#Now load and call the specific code for this pico
+main = __import__(pico)
+main.main(client)
