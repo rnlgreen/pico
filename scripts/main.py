16a7f3a3f3a3ad0b7d0a7ef6877a7d6003af4d91
Richard <richard.n.l.green@gmail.com>
2022-10-30 16:59:13 +0000

    Added exception handling for NTP and changed pool.

--- scripts/main.py
+++ scripts/main.py
@@ -88,7 +88,8 @@ print("I am {}".format(pico))
 ipaddr = wifi.wlan_connect(pico)
 if ipaddr:
     #Sync the time up
-    ntp.set_time()
+    if not ntp.set_time():
+        print("Failed to set the time")
 
     #Try and connect to MQTT
     client = mqtt.mqtt_connect(client_id=pico)
--- scripts/utils/ntp.py
+++ scripts/utils/ntp.py
@@ -5,7 +5,7 @@ import struct
 import machine # type: ignore
 
 NTP_DELTA = 2208988800
-host = "pool.ntp.org"
+host = "0.uk.pool.ntp.org"
 
 def set_time():
     NTP_QUERY = bytearray(48)
@@ -13,12 +13,17 @@ def set_time():
     addr = socket.getaddrinfo(host, 123)[0][-1]
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.settimeout(1)
+        s.settimeout(2)
         res = s.sendto(NTP_QUERY, addr)
         msg = s.recv(48)
+    except Exception as e:
+        print("Exception getting NTP: {}".format(e))
+        return False
     finally:
         s.close()
+    print("NTP fetch was succesful")
     val = struct.unpack("!I", msg[40:44])[0]
     t = val - NTP_DELTA    
     tm = time.gmtime(t)
     machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+    return True
