d14010ee050c9a5a6ccc7206b7a83760b4280f15
Richard <richard.n.l.green@gmail.com>
2022-10-30 19:00:41 +0000

    Tidied up

---
+++ scripts/secrets_sample.py
@@ -0,0 +1,7 @@
+#WiFi Credentials
+ssid = "SSID"
+wlan_pass = "PASSPHRASE"
+mqtt_server = "MQTTSERVER"
+ftphost = "FTPHOST"
+ftpuser = "FTPUSER"
+ftppw = "FTPPASSWORD"
--- secrets.py
+++ secrets.py
@@ -1,6 +1,10 @@
 #WiFi Credentials
+#ssid = "osprey"
+#wlan_pass = "birdsofprey"
 ssid = "owl"
 wlan_pass = "dodgyiot"
+#ssid = "W3Z-83B3"
+#wlan_pass = "Herdyhigh07"
 mqtt_server = "condor"
 ftphost = "condor"
 ftpuser = "piftp"
