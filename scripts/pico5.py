01f6f4368b4e7c6bdcdaa2eedf0afa66b4b1ee3c
Richard <richard.n.l.green@gmail.com>
2023-04-16 12:44:15 +0100

    Added hysteresis logic

--- scripts/pico5.py
+++ scripts/pico5.py
@@ -7,7 +7,7 @@ import utils.leds as leds
 from machine import Pin, ADC # type: ignore
 
 photoPIN = 26 #GPIO26, Pin 31
-debugging = False
+debugging = True
 
 #Print and send status messages
 def status(message):
@@ -68,12 +68,25 @@ def rolling_average():
     latest = readLight()
     readings += [latest]
     avg = sum(readings)/len(readings)
-    debug("Latest: {} Average: {}".format(latest,avg))
+    #debug("Latest: {} Average: {} Hysteresis: {}".format(latest,avg))
     return(avg)
+
+#Returns new brightness level
+def get_brightness(lightlevel):
+    b = min(80,max(10,(round((lightlevel * 2)/5) * 5) - 10))
+    return b
+
+#Calculate how close the new brightness is to a threshold
+def check_hysteresis(lightlevel):
+    l = lightlevel / 2.5
+    h = 0.5 - abs(l - round(l,0))
+    debug("Hysteresis {} -> {}".format(lightlevel,h))
+    return(h)
 
 #Control LEDs based on light and time of day
 #Returns True if lights were updated so we can slow the rate of changes
 def manage_lights():
+    #Get the latest rolling average light level
     lightlevel = rolling_average()
     #Check time of day first
     hour = utime.localtime()[3]
@@ -83,30 +96,40 @@ def manage_lights():
         hour += 1
         if (hour == 23):
             hour = 0
+    #Flag whether we changed the lights or not
     updated = False
-    #Turn on or adjust brightness for low light level, unless it is late
+    #Only manage lights between certain hours
+    DIM = 45
+    BRIGHT = 50 #(was 55)
     if hour >= 6 or hour < 2 : #from 06:00 to 01:59
-        if lightlevel < 45:
+        #Turn off for high light levels
+        if lightlevel > BRIGHT and not leds.lightsoff:
+            status("Turning lights off")
+            send_control("off")
+            updated = True
+        #Turn on or adjust for low light levels
+        elif lightlevel < DIM:
+            #If the lights are off then we've just dropped below DIM and need to turn them on
             if leds.lightsoff:
                 status("Turning lights on")
                 if leds.colour == [0, 0, 0]:
                     send_control("rgb(0, 255, 255)")
                     updated = True
-                #Set brightness to 50% of target, we'll see if we need brighter as the rolling average catches up
-                new_brightness = min(80,max(10,(round((lightlevel)/5) * 5) - 10))
-            else:
-                #New brightness something between 10 and 80 step 5
-                new_brightness = min(80,max(10,(round((lightlevel*2)/5) * 5) - 10))
+                ##Set brightness to 50% of target, we'll see if we need brighter as the rolling average catches up
+                #new_brightness = get_brightness(lightlevel/2)
+            #Otherwise just calculate a new brightness level
+            #else:
+            #New brightness something between 10 and 80 step 5
+            new_brightness = get_brightness(lightlevel)
+            #If the brightness level has changed check for hysteresis 
             if leds.brightness != new_brightness:
-                debug("Old brightness: {} -> New brightness: {}".format(leds.brightness,new_brightness))
-                status("Brightness {} -> {}".format(leds.brightness,new_brightness))
-                send_control("brightness:{}".format(new_brightness))
-                updated = True
-        #Turn off for high light levels
-        if lightlevel > 55 and not leds.lightsoff:
-            status("Turning lights off")
-            send_control("off")
-            updated = True
+                if abs(new_brightness - leds.brightness) > 5 \
+                or check_hysteresis(lightlevel) > 0.1:
+                    status("Brightness {} -> {}".format(leds.brightness,new_brightness))
+                    send_control("brightness:{}".format(new_brightness))
+                    updated = True
+                else:
+                    debug("Skipping light change from {} to {} to avoid flutter, brightness: {}".format(leds.brightness,new_brightness,lightlevel))
     elif not leds.lightsoff: #If out of control hours then turn off
         status("Turning lights off")
         send_control("off")
@@ -142,7 +165,7 @@ def main():
                     #Manage LEDs based on light level and time of day
                     updated = manage_lights()
                 else:
-                    debug("Skipping lighting update this time")
+                    debug("Waiting for lights to fade")
                     updated = False
                     rolling_average()
             else:
