fd7dc2fca50128829b9914e57a2f901b9168b8bd
Richard <richard.n.l.green@gmail.com>
2026-03-08 10:12:06 +0000

    Disable slack until Virgin get here

--- scripts/utils/slack.py
+++ scripts/utils/slack.py
@@ -2,23 +2,25 @@
 from secrets import webhook
 import urequests # type: ignore # pylint: disable=import-error
 from utils import log
+do_slack=False
 
 #Format and send the message to Slack
 def send_msg(pico,msg):
-    ''' Send a message to a predefined slack channel.'''
-    URL=webhook
-    headers = {'content-type': 'application/json'}
-    if "exception" in msg:
-        icon = ":large_red_square:"
-    elif "is up" in msg:
-        icon = ":large_green_circle:"
-    else:
-        icon = ":large_blue_diamond:"
-    data = f"{{'text':'{msg}',  'username': '{pico}', 'icon_emoji': '{icon}'}}"
-    try:
-        urequests.post(URL, data=data, headers=headers)
-    except Exception as oops: # pylint: disable=broad-except
-        log.status("Failed to send message to Slack", logit=True, handling_exception=True)
-        log.log_exception(oops)
-        return False
+    if do_slack:
+        ''' Send a message to a predefined slack channel.'''
+        URL=webhook
+        headers = {'content-type': 'application/json'}
+        if "exception" in msg:
+            icon = ":large_red_square:"
+        elif "is up" in msg:
+            icon = ":large_green_circle:"
+        else:
+            icon = ":large_blue_diamond:"
+        data = f"{{'text':'{msg}',  'username': '{pico}', 'icon_emoji': '{icon}'}}"
+        try:
+            urequests.post(URL, data=data, headers=headers)
+        except Exception as oops: # pylint: disable=broad-except
+            log.status("Failed to send message to Slack", logit=True, handling_exception=True)
+            log.log_exception(oops)
+            return False
     return True
